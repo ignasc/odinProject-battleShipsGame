3e13f4f3223c646ce8fb503f29cb05651fa98550
Ignas C <15948796+ignasc@users.noreply.github.com>
2025-12-23 12:01:26 +0000

    Add game board class functions.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,5 +1,95 @@
+import Ship from "./ship";
+
 class GameBoard {
-    constructor() {}
+    constructor() {
+        this.ships = [];
+        this.board = [];
+        this.positionsFired = [];
+        this.missedAttacks = 0;
+        for (let i = 0; i < 10; i++) {
+            const row = [];
+            for (let j = 0; j < 10; j++) {
+                row.push(0);
+            }
+            this.board.push(row);
+        }
+    }
+
+    getPositionContents(coordX, coordY) {
+        return this.board[coordY][coordX];
+    }
+
+    spawnShip(shipLength, coordX, coordY, isRotated90, playerOwner) {
+        const newShip = new Ship(
+            shipLength,
+            coordX,
+            coordY,
+            isRotated90,
+            playerOwner
+        );
+        //check if ship is within board boundaries
+        if (isRotated90) {
+            if (coordY + shipLength < 10) {
+                this.ships.push(newShip);
+
+                //reserve positions on board by referencing the ship instance
+                for (let i = 0; i < shipLength; i++) {
+                    this.board[coordY + i][coordX] = newShip;
+                }
+            }
+        } else {
+            if (coordX + shipLength < 10) {
+                this.ships.push(newShip);
+
+                //reserve positions on board by referencing the ship instance
+                for (let i = 0; i < shipLength; i++) {
+                    this.board[coordY][coordX + i] = newShip;
+                }
+            }
+        }
+    }
+
+    receiveAttack(coordX, coordY) {
+        //return codes:
+        //  -1 = missed
+        //  0 = already fired before
+        //  1 = hit
+        //check if position was already fired at
+        for (let i = 0; i < this.positionsFired.length; i++) {
+            const position = this.positionsFired[i];
+            if (position[1] === coordX && position[0] === coordY) {
+                return 0;
+            }
+        }
+        this.positionsFired.push([coordY, coordX]);
+        const target = this.getPositionContents(coordX, coordY);
+
+        if (target instanceof Ship) {
+            target.hit();
+            return 1;
+        } else {
+            this.missedAttacks++;
+            return -1;
+        }
+    }
+
+    getMissedAttackCount() {
+        return this.missedAttacks;
+    }
+
+    areAllShipsDestroyed() {
+        // -1 = no ships spawned
+        if (this.ships.length === 0) {
+            return -1;
+        }
+        for (let i = 0; i < this.ships.length; i++) {
+            const ship = this.ships[i];
+            if (!ship.isSunk()) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 export default GameBoard;
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,8 +1,59 @@
 import GameBoard from "./gameboard.js";
+import Ship from "./ship.js";
 
 describe("Testing game board", () => {
     test("Game board is created", () => {
         const gameBoard = new GameBoard();
         expect(gameBoard).toBeInstanceOf(GameBoard);
+    });
+    test("Spawn a ship", () => {
+        const gameBoard = new GameBoard();
+        gameBoard.spawnShip(2, 2, 2, true, "player1");
+        gameBoard.spawnShip(4, 0, 0, false, "player1");
+        expect(gameBoard.ships[0]).toBeInstanceOf(Ship);
+    });
+    test("Test ship position and reference on board", () => {
+        const gameBoard = new GameBoard();
+        const shipLength = 3;
+        gameBoard.spawnShip(shipLength, 2, 2, true, "player1");
+        expect(gameBoard.getPositionContents(2, 2)).toBeInstanceOf(Ship);
+        expect(gameBoard.getPositionContents(2, 3)).toBeInstanceOf(Ship);
+        expect(gameBoard.getPositionContents(2, 4)).toBeInstanceOf(Ship);
+    });
+    test("Shoot the ship", () => {
+        const gameBoard = new GameBoard();
+
+        const shipLength = 3;
+        expect(gameBoard.areAllShipsDestroyed()).toBe(-1);
+        gameBoard.spawnShip(shipLength, 2, 2, true, "player1");
+        expect(gameBoard.areAllShipsDestroyed()).toBe(false);
+        gameBoard.receiveAttack(2, 4); //hit ship
+        expect(gameBoard.getPositionContents(2, 2).isSunk()).toBe(false);
+        gameBoard.receiveAttack(2, 2); //hit ship
+        expect(gameBoard.getPositionContents(2, 2).isSunk()).toBe(false);
+        gameBoard.receiveAttack(2, 2); //hit the same spot (not counted)
+        expect(gameBoard.getPositionContents(2, 2).isSunk()).toBe(false);
+        gameBoard.receiveAttack(2, 3); //hit ship
+        expect(gameBoard.getPositionContents(2, 2).isSunk()).toBe(true);
+        expect(gameBoard.areAllShipsDestroyed()).toBe(true);
+
+        expect(gameBoard.getMissedAttackCount()).toBe(0);
+
+        gameBoard.spawnShip(shipLength, 0, 0, false, "player1");
+        expect(gameBoard.areAllShipsDestroyed()).toBe(false);
+        gameBoard.receiveAttack(2, 0);
+        expect(gameBoard.getPositionContents(2, 0).isSunk()).toBe(false);
+        gameBoard.receiveAttack(2, 0);
+        expect(gameBoard.getPositionContents(2, 0).isSunk()).toBe(false);
+        gameBoard.receiveAttack(1, 0);
+        expect(gameBoard.getPositionContents(1, 0).isSunk()).toBe(false);
+        gameBoard.receiveAttack(0, 0);
+        expect(gameBoard.getPositionContents(0, 0).isSunk()).toBe(true);
+        expect(gameBoard.areAllShipsDestroyed()).toBe(true);
+
+        gameBoard.receiveAttack(8, 8); //missed attack
+        expect(gameBoard.getMissedAttackCount()).toBe(1);
+        gameBoard.receiveAttack(7, 8); //missed attack
+        expect(gameBoard.getMissedAttackCount()).toBe(2);
     });
 });
