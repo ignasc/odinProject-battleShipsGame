459fad0ea4bf7ca21e1cc74e631c2f4ff28636ba
Ignas <15948796+ignasc@users.noreply.github.com>
2026-01-05 11:16:38 +0000

    Add a basic start menu to set up player one and two.
    This includes options to select if player two is computer.
    And ability to select the dificulty of computer controlled opponene.
    
    Note: this implementation is only visual, menu layout.

--- src/index.js
+++ src/index.js
@@ -8,23 +8,26 @@ import GameUI, {
     createGameBoard,
     updatePositionStatus,
 } from "./ui_gameboard_template.js";
+import startMenu from "./ui_startMenu.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const playerOne = new Player(true, "Ignas", 1);
-    const playerTwo = new Player(false, "Second Ignas", 2);
-    const game = new GameEngine(playerOne, playerTwo);
-    const gameUI = new GameUI(playerOne, playerTwo, game);
-
-    //initial game settings
-    playerTwo.getBoard().toggleBoard();
-
+    const form = startMenu;
     const btnStartGame = document.createElement("button");
     btnStartGame.addEventListener("click", () => {
+        const playerOne = new Player(true, "Ignas", 1);
+        const playerTwo = new Player(false, "Second Ignas", 2);
+        const game = new GameEngine(playerOne, playerTwo);
+        const gameUI = new GameUI(playerOne, playerTwo, game);
+
+        //hide second player board (this needs to be removed/fixed to not need it anymore)
+        // playerTwo.getBoard().toggleBoard();
+
         gameUI.updateUI();
     });
     btnStartGame.textContent = "start game";
+    document.getElementById("mainApp").appendChild(form);
     document.getElementById("mainApp").appendChild(btnStartGame);
 
     // automatically trigger button for debug purposes.
-    btnStartGame.click();
+    // btnStartGame.click();
 });
