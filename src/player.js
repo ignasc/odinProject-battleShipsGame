6a1f1dbf43d13a95ce15c7cdd03e1853531d860e
Ignas <15948796+ignasc@users.noreply.github.com>
2026-01-05 10:11:19 +0000

    Add shuffle algorithm name.

--- src/player.js
+++ src/player.js
@@ -67,6 +67,7 @@ class Player {
     }
 
     #shuffleArray(array) {
+        // Fisher–Yates shuffle algorithm
         for (let i = array.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
 
