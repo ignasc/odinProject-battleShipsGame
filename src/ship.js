c444eb796e989567f178bcad4bbc33c0fc45c6e0
Ignas <15948796+ignasc@users.noreply.github.com>
2026-01-27 16:05:32 +0000

    Add ship sprite placeholders and apply them to game boards.
    Temporary ship sprites added and they are correctly applied to ships
    based on their conditions: healthy, damaged, destroyed.

--- src/ship.js
+++ src/ship.js
@@ -1,12 +1,37 @@
 class Ship {
-    constructor(length) {
+    constructor(length, coordX, coordY, isRotated90, playerOwner) {
         this.length = length;
+        this.coordX = coordX;
+        this.coordY = coordY;
         this.hits = 0;
+        this.isRotated90 = isRotated90;
+        this.shipPositions = [];
+    }
+
+    storePosition(coordX, coordY) {
+        console.log(`Storing ship position ${coordX}:${coordY}`);
+        this.shipPositions.push([coordX, coordY]);
+    }
+
+    getShipPositions() {
+        return this.shipPositions;
+    }
+
+    isRotated() {
+        return this.isRotated90;
+    }
+
+    getLength() {
+        return this.length;
     }
 
     hit() {
         this.hits++;
     }
+    isDamaged() {
+        return this.hits != 0 && this.hits < this.length ? true : false;
+    }
+
     isSunk() {
         return this.hits >= this.length ? true : false;
     }
